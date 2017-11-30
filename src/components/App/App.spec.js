8385262a8301ae896fbefb23ca885526e0cb3d11
Erez Makavy <hepi.yellow@gmail.com>
2017-11-30 19:46:54 +0200

    Add unit-test for data filtering

--- src/components/App/App.spec.js
+++ src/components/App/App.spec.js
@@ -14,4 +14,14 @@ describe('App', () => {
     );
     expect(wrapper.find('h2').length).to.eq(1);
   });
+
+  it('filters data according to filter string', () => {
+    const data = [
+      {title: 'abc'},
+      {title: 'afg'}
+    ];
+    expect(App.getFilteredData(data, 'rer', 'title').length).to.eq(0);
+    expect(App.getFilteredData(data, 'ab', 'title').length).to.eq(1);
+    expect(App.getFilteredData(data, 'a', 'title').length).to.eq(2);
+  });
 });
